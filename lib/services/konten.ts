1d0f4b917c8d5007f104148924c346c0bf2c401c
EgiUF <egiumarf1@gmail.com>
2026-05-07 13:02:43 +0700

    error env, sama 'use server'

--- lib/services/konten.ts
+++ lib/services/konten.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { createClient } from '@/lib/supabase/client';
 import { prisma } from '@/lib/prisma';
 
--- lib/services/workspace.ts
+++ lib/services/workspace.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { prisma } from '@/lib/prisma';
 
 function serializeData(obj: any): any {
